38009d7afea17cd0c730c2392b02c82338e1e438
AlexanderJCS <alexandercastronovo@gmail.com>
2025-11-03 21:23:05 -0500

    Remove unused code and prefab thing

--- Assets/InputManager.cs
+++ Assets/InputManager.cs
@@ -1,21 +1,14 @@
-using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class InputManager : MonoBehaviour
 {
     [SerializeField] private InputActionMap inputs;
-    private AudioSource audioSource;
     [SerializeField] private AudioClip[] buttonSounds;
-
-    [SerializeField] private uint[] polyrhythms;
     [SerializeField] private CatSpinner[] cats;
-    public float bpm = 60;  // where the beat aligns on the 2 polyrhythm
-    
+
     private void Start()
     {
-        audioSource = GetComponent<AudioSource>();
-        
         for (int i = 1; i <= 5; i++)
         {
             int index = i;
