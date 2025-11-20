4b4d60f31489d872a94464661c548700889c1a9e
Makai P. <makaipindell@gmail.com>
2025-11-20 10:19:51 -0500

    e

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using TMPro;
+
+public class MainMenu : MonoBehaviour
+{
+    public TMP_Text difficultytext; 
+    
+    public enum Difficulty { NORMAL, AUTOPLAY, FREESTYLE }
+    Difficulty currentDifficulty = Difficulty.NORMAL;
+
+    public void ToggleDifficulty()
+    {
+        if(difficultytext == null) 
+        {
+            Debug.LogError("Difficulty Text is not assigned in the Inspector!");
+            return;
+        }
+
+        switch (currentDifficulty)
+        {
+            case Difficulty.NORMAL:
+                currentDifficulty = Difficulty.AUTOPLAY;
+                difficultytext.text = "‹ AUTOPLAY ›";
+                //GameModeMode.gameMode = GameMode.AUTOPLAY;
+                break;
+            case Difficulty.AUTOPLAY:
+                currentDifficulty = Difficulty.FREESTYLE;
+                difficultytext.text = "‹ FREESTYLE ›";
+
+                //GameModeMode.gameMode = GameMode.FREESTYLE;
+                break;
+            case Difficulty.FREESTYLE:
+                currentDifficulty = Difficulty.NORMAL;
+                difficultytext.text = "‹ NORMAL ›";
+
+                //GameModeMode.gameMode = GameMode.NORMAL;
+                break;
+        }
+    }
+}
